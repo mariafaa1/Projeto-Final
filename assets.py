767693e7170be5ea3812f45f41faac28cfe10ca7
Rafael Bulgarelli <rafael@notions.digitalm>
2025-05-13 11:53:32 -0300

    corações e vidas no jogo

--- assets.py
+++ assets.py
@@ -7,6 +7,7 @@ def carregar_recursos():
     recursos['jogador1'] = pygame.image.load('assets/player1.png').convert_alpha()
     recursos['jogador2'] = pygame.image.load('assets/player2.png').convert_alpha()
     recursos['bala'] = pygame.image.load('assets/bullet.png').convert_alpha()
+    recursos['coracao'] = pygame.image.load('assets/coracao.png').convert_alpha()  # <-- ESTA LINHA É ESSENCIAL
 
     # Sons
     recursos['som_tiro'] = pygame.mixer.Sound('assets/MA_Designed_ModifiedGunBlasts_2.wav')
